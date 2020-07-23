da51cd30e494389857edc8f3713274755ffa9081
Farshad <Farshad.ftf@gmail.com>
2020-07-23 19:12:39 +0430

    Initial commit

---
+++ src/test/java/com/farshad/springit/SpringitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farshad.springit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
